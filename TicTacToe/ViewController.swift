74138068ea54842ebef2e6208b216cd899c500f9
Xizafren <m-ett@hotmail.se>
2020-12-16 12:36:11 +0100

    board working

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -10,16 +10,29 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var buttons: [UIButton]!
+   //TODO: creating a board
+     var board = [String]()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //testing board
+        loadBoard()
+        print(board)
         // Do any additional setup after loading the view.
     }
 
     @IBAction func buttonPressed(_ sender: UIButton) {
         let index = buttons.index(of: sender)!
         print(index)
+    }
+    
+    // Initialize the board
+    //size of the board the size of the buttons
+    func loadBoard(){
+        for i in 0..<buttons.count {
+            board.append("")
+        }
     }
     
 }
