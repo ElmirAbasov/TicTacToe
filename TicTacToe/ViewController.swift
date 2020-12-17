5a13c2e7dcc125eb199c653080e64e0fd99b0f9f
Xizafren <m-ett@hotmail.se>
2020-12-17 14:11:52 +0100

    ...

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -12,6 +12,14 @@ class ViewController: UIViewController {
     @IBOutlet var buttons: [UIButton]!
    //TODO: creating a board
      var board = [String]()
+    // Placing a player on the board
+    var currentPlayer = ""
+    //TODO: Set up rules of the game
+    var rules = [[0, 1, 2],[3,4,5],
+                 [6, 7, 8],[0,3,6],
+                 [1, 4, 7],[2,5,8],
+                 [0, 4, 8],[2,4,6]]
+    
     
     
     override func viewDidLoad() {
@@ -24,7 +32,40 @@ class ViewController: UIViewController {
 
     @IBAction func buttonPressed(_ sender: UIButton) {
         let index = buttons.index(of: sender)!
-        print(index)
+       // print(index)
+        
+        
+        //Setting up so u cant click twice on the same button
+        if !board[index].isEmpty {
+            return
+        }
+        
+        if currentPlayer == "X" {
+            sender.setTitle("X", for: .normal)
+            currentPlayer = "O"
+            board[index] = "X"
+        }else{
+            sender.setTitle("O", for: .normal)
+            currentPlayer = "X"
+            board[index] = "O"
+            
+        }
+        whoWins()
+        
+    }
+    
+    func whoWins() {
+        for rule in rules {
+          let playerat0 = board[rule[0]]
+            let playerat1 = board[rule[1]]
+            let playerat2 = board[rule[2]]
+            
+            if playerat0 == playerat1,
+               playerat1 == playerat2,
+               !playerat0.isEmpty {
+                print ("Winner is \(playerat0)")
+            }
+        }
     }
     
     // Initialize the board
